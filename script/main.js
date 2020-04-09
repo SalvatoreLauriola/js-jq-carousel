0b048d5d4c16ff974f33bb6a7cb4c6a2dbe1cd8b
Salvatore Lauriola <salvatore6324@gmail.com>
2020-04-09 18:46:53 +0200

    Next step

---
+++ script/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+
+
+// references
+
+var btnDx = $('.icon-dx > i');
+console.log(btnDx);
+var btnSx = $('.icon-sx > i');
+var imageFirst = $('.first-img');
+
+
+
+
+btnDx.click( function(){
+
+ 
+  imageFirst.show();
+})
+
+
+
+
+// btnDx.click( function () {
+
+  
+//   imageFirst.hide();
+// console.log(btnDx(click));
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});  //end ready
