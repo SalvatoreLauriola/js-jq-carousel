1da308335b9f604f77c672cf48260c5d171e2024
Salvatore Lauriola <salvatore6324@gmail.com>
2020-04-09 23:03:18 +0200

    finished without keycode

--- script/main.js
+++ script/main.js
@@ -6,28 +6,46 @@ $(document).ready(function(){
 
 var btnDx = $('.icon-dx > i');
 var btnSx = $('.icon-sx > i');
-var img = $('.wallpaper img')
 
+var firstImg = $('.first');
+var lastImg = $('.last');
+
+var firstCircle = $('.first2');
+var lastCircle = $('.last2');
 
 
 btnDx.click(function (){
   var selected = $('.wallpaper img.active')
-  if (selected.hasClass('active')) {
-
-    selected.removeClass('active');
-    selected.next().addClass('active');
   
+  
+  selected.removeClass('active');
+  selected.next().addClass('active');
+  if (selected.hasClass('last')){
+    firstImg.addClass('active');
+    
   }
-  
+  var selected2 = $('.circlebox .circle.active2')
+  selected2.removeClass('active2');
+  selected2.next().addClass('active2');
+  if (selected2.hasClass('last2')){
+    firstCircle.addClass('active2');
+  }
 })
 
 btnSx.click(function (){
   var selected = $('.wallpaper img.active')
-  if (selected.hasClass('active')) {
+ 
 
-    selected.removeClass('active');
-    selected.prev().addClass('active');
-  
+  selected.removeClass('active');
+  selected.prev().addClass('active');
+  if (selected.hasClass('first')){
+    lastImg.addClass('active');
+  }
+  var selected2 = $('.circlebox .circle.active2')
+  selected2.removeClass('active2');
+  selected2.prev().addClass('active2');
+  if (selected2.hasClass('first2')){
+    lastCircle.addClass('active2');
   }
   
 })
